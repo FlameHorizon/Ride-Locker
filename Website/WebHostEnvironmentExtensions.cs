7efce0644f39ffaa2bedf9fd1db07d1b7feafe22
FlameHorizon <maciejpiotrowsk@gmail.com>
2026-01-03 10:08:52 +0100

    Fix for getting relative path of file in wwwroot.

--- Website/WebHostEnvironmentExtensions.cs
+++ Website/WebHostEnvironmentExtensions.cs
@@ -1,12 +1,5 @@
 public static class WebHostEnvironmentExtensions {
-  public static string GetRelativeWebPath(this IWebHostEnvironment env, string path) {
-    string res = path.Replace(env.WebRootPath, "").Replace("\\", "/");
-
-    // Ensure leading slash
-    if (!res.StartsWith("/")) {
-      res = "/" + res;
-    }
-
-    return res;
+  public static string GetRelativeWebPath(this IWebHostEnvironment env, string physicalPath) {
+    return "/" + Path.GetRelativePath(env.WebRootPath, physicalPath).Replace("\\", "/");
   }
 }
